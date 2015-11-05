5b943c72eaf92b378be13afb355e5ed622ab682d
Brea Gaudioso <breagaudioso@gmail.com>
2015-11-04 18:25:52 -0800

    built factory. added more functionality

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,10 +1,23 @@
 var app = angular.module('TeaShopApp');
 
-app.controller('ShopCtrl', ['$scope', '$http', function($scope, $http){
-  $scope.hey = "Heyyyyyyy";
-  $http.get('../tea.json').then(function(data){
-    $scope.teas = data.data;
-    console.log($scope.teas);
-  })
-}])
+app.controller('ShopCtrl', ['$scope', 'TeaFactory', 'CartFactory', function($scope, TeaFactory, CartFactory){
+    $scope.teas = Teas.teas;
+    $scope.cart = []
+    $scope.quanitity = 0
+    $scope.categories =  ['dark', 'cold', 'awesome', 'dry', 'hot', 'warm', 'summer', 'winter', 'spring', 'lucid'];
+    $scope.addToCart = function(id, number) {
+      if (tea !== undefined) {
+        tea = {teaId: id, quantity: number}
+        $scope.cart.push(tea);
+        $scope.quantity += parseInt(tea);
+        // this doesnt work still
+      }
+      return $scope.cart
+    };
+}]);
 
+
+app.controller('CheckOutCtrl', ['$scope', '$http', function($scope, $http){
+
+
+}])
--- app/js/services.js
+++ app/js/services.js
@@ -1,7 +1,23 @@
-// var app = angular.module('TeaShopApp');
+var app = angular.module('TeaShopApp');
 
-// app.factory("TeaFactory", ['$http', function($http){
+app.factory("TeaFactory", ['$http', function($http){
+  Teas = {};
+  Teas.teas = [];
+  $http.get('../tea.json').then(function(data) {
+    var allTea = data.data
+    console.log(allTea)
+    allTea.forEach(function(tea) {
+      Teas.teas.push(tea);
+    });
+  });
+  return Teas;
+}]);
 
-// }]);
+app.factory('CartFactory', [function() {
+  var Cart = {};
+  Cart.cart =[];
+  return Cart;
+}]);
+
 
 
